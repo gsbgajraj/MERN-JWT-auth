d421a140dba0c45fc8310cdc449843a3917aa844
Gajraj Singh Bhati <itsgajraj.in@gmail.com>
2025-02-18 12:25:39 +0530

    server.js update

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ import { errorHandler } from './middlewares/error.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import path from 'path';
 
 dotenv.config();
+const app = express();
+
 
 connectDB();
 
-const app = express();
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const buildPath = path.join(__dirname, './client/build');
+app.use(express.static(buildPath));
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,6 +29,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/auth', authRoutes);
 
 app.use(errorHandler);
+
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, './', 'client', 'build', 'index.html')); });
+
 
 const server = app.listen(PORT, () =>
   console.log(`Server started listening on ${PORT}`)
